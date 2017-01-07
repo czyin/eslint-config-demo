862de6f87ca038693cd3c1607799ab877ccf6aab
czyin <chengzhi.yin@outlook.com>
2017-01-07 23:35:40 +0800

    add es5 configs

---
+++ es5.js
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: [
+    './rules/errors.js',
+    './rules/best-practices.js',
+    './rules/strict.js',
+    './rules/variables.js',
+    './rules/node.js',
+    './rules/style.js'
+  ].map(require.resolve),
+  parserOptions: {
+    ecmaVersion: 2017,
+    sourceType: 'module',
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+    },
+  },
+  rules: {
+    strict: 'error'
+  }
+}
